9a29177c9129e0ccdd4d8d6902c2f6a02261ac99
pragyamat <70574942+pragyamat@users.noreply.github.com>
2020-09-02 00:22:07 +0530

    Assignment successful

---
+++ Day2/ques4.js
@@ -0,0 +1,8 @@
+let names = ["George","Madeline","Ramesh","Avantika"];
+for(let i=0 ; i<names.length ; i++)
+{
+    if( names[i].includes("a"))
+        {
+            console.log(names[i]);
+        }
+}
