f66558e3f6ec3c486c8f54cfa37bc9aa1c3a324d
pragyamat <70574942+pragyamat@users.noreply.github.com>
2020-09-02 00:31:27 +0530

    Update ques4.js

--- Day2/ques4.js
+++ Day2/ques4.js
@@ -1,3 +1,5 @@
+// Question-4
+//Program to display only elements containing 'a' in them from a array 
 let names = ["George","Madeline","Ramesh","Avantika"];
 for(let i=0 ; i<names.length ; i++)
 {
