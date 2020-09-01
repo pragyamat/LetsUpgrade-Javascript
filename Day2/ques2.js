fdca362aa05786d1fea1f7ceeb84bf3a1136ceb5
pragyamat <70574942+pragyamat@users.noreply.github.com>
2020-09-02 00:50:53 +0530

    Answer 2 submitted

---
+++ Day2/ques2.js
@@ -0,0 +1,5 @@
+// Question-2
+// Program to convert minutes to seconds.
+let minutes = 6;
+let seconds = (minutes*60);
+console.log(`If minutes is ${minutes} then after converting to seconds it will be ${seconds}`);
